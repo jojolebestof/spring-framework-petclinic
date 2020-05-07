ce4b47eb8964eea705aeb50c2cef635e560e5b0d
Herve <dockerlite@gmail.com>
2020-05-07 06:18:56 +0200

    try to open a file in jenkins

---
+++ cat.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#!/bin/bash
+#
+# Test the JMeter Docker image using a trivial test plan.
+
+# Example for using User Defined Variables with JMeter
+# These will be substituted in JMX test script
+# See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
+export TARGET_HOST="www.map5.nl"
+export TARGET_PORT="80"
+export TARGET_PATH="/kaarten.html"
+export TARGET_KEYWORD="Kaartdiensten"
+
+T_DIR=${PWD}/tests/trivial
+
+cat  ${T_DIR}/test-plan.jmx 
+
