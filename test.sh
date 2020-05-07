db220b50982182ba75941f7521ead01ad2666dff
Herve <dockerlite@gmail.com>
2020-05-07 05:46:17 +0200

    Added jmeter test file and bash shell

--- test.sh
+++ test.sh
@@ -19,7 +19,7 @@ mkdir -p ${R_DIR}
 
 /bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
 
-./run.sh -Dlog_level.jmeter=DEBUG \
+/bin/bash run.sh -Dlog_level.jmeter=DEBUG \
 	-JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
 	-JTARGET_PATH=${TARGET_PATH} -JTARGET_KEYWORD=${TARGET_KEYWORD} \
 	-n -t ${T_DIR}/test-plan.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
